d155f0ac1e2c768bd552aefe1b91371bd4e53885
Arooma <aroomakhan1@outlook.com>
2024-05-15 15:59:44 +1000

    complete

--- public/js/script.js
+++ public/js/script.js
@@ -1,74 +1,70 @@
-const cardList = [ 
-    { 
-    title: "Caret", 
-    image: "images/@soffairys.jpeg",  
-    link: "About Caret", 
-    description: "basic information about Caret" 
-    },  
-    { 
-    title: "Le Relais de l’Entrecôte", 
-    image: "images/Red wine, entrecote and french fries @ Le Relais de L'entrecôte - Paris, France.jpeg", 
-    link: "About Le Relais de l’Entrecôte", 
-    description: "basic information about Le Relais De L’Entrecôte" 
-    }, 
-    { 
-    title: "Cedric Grolet",  
-    image: "images/croissant.jpeg", 
-    link: "About Cedric Grolet", 
-    description: "basic information about Cedric Grolet", 
-    } 
-    ]; 
-
 const addCards = (items) => {
     items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">' +
-            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.image + '">' +
-            '</div><div class="card-content">' +
-            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.link + '</a></p></div>' +
-            '<div class="card-reveal">' +
-            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
-            '<p class="card-text card-desc-color">' + item.desciption + '</p>' +
-            '</div></div></div>';
-        $("#card-section").append(itemToAppend)
+        let itemToAppend = `
+            <div class="col s4 center-align">
+                <div class="card medium">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="${item.image}">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title activator grey-text text-darken-4">${item.title}<i class="material-icons right">more_vert</i></span>
+                        <p><a href="#">${item.link}</a></p>
+                    </div>
+                    <div class="card-reveal">
+                        <span class="card-title grey-text text-darken-4">${item.title}<i class="material-icons right">close</i></span>
+                        <p class="card-text card-desc-color">${item.description}</p>
+                    </div>
+                </div>
+            </div>`;
+        $("#card-section").append(itemToAppend);
+    });
+};
+
+function formSubmitted() {
+    let formData = {
+        first_name: $('#first_name').val(),
+        last_name: $('#last_name').val(),
+        review: $('#review').val(),
+        email: $('#email').val()
+    };
+
+    console.log(formData);
+    postParis(formData);
+};
+
+function postParis(formData) {
+    $.ajax({
+        url: '/api/paris',
+        type: 'POST',
+        data: formData,
+        success: (result) => {
+            if (result.statusCode === 200) {
+                alert('Successfully posted Paris data. Horray');
+            }
+        },
+        error: (err) => {
+            console.error('Error in posting the Paris data:', err);
+            alert('Error in Posting Data :(');
+        }
     });
 }
 
-const clickMe = () => {
-    alert("Have a great day guys!")
-}
-
-const submitForm = () => {
-    let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
-
-    console.log("Form Data Submitted: ", formData);
+function getAllParis() {
+    $.get('/api/paris', (response) => {
+        if (response.statusCode === 200) {
+            addCards(response.data);
+        }
+    });
 }
 
 $(document).ready(function () {
     $('.materialboxed').materialbox();
-    addCards(cardList);
-    $('.modal').modal();
     $('#formSubmit').click(() => {
-        submitForm();
-    })
-})
-
-const getParis = () => {
-    $.get('/api/paris', (response) => {
-        if(response.statusCode==200) {
-            addParis (response.data);
-        }
-    })
-}
-
-$(document).ready(function() {
-    $('.materialboxed').materialbox();
-    $('#formSubmit').click(()=> {
-        submitForm();
-    })
-    getProjects();
+        formSubmitted();
+    });
+    $('#clickMeButton').click(() => {
+        $('.modal').modal('open'); 
+    });
+    getAllParis();
     $('.modal').modal();
 });
--- server.js
+++ server.js
@@ -1,16 +1,100 @@
 var express = require ("express") 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 var app = express ()
-app.use(express.static(__dirname+'/public'))
-app.use(express.json()); app.use(express.urlencoded({extended: false}));
+const uri = "mongodb+srv://aroomakhan:monday122@cluster0.uklxfa9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 var port = process.env.port || 5500;
 
+const cardList = [ 
+    { 
+        title: "Caret", 
+        image: "images/@soffairys.jpeg",  
+        link: "About Caret", 
+        description: "Basic information about Caret" 
+    },  
+    { 
+        title: "Le Relais de l’Entrecôte", 
+        image: "images/Red wine, entrecote and french fries @ Le Relais de L'entrecôte - Paris, France.jpeg", 
+        link: "About Le Relais de l’Entrecôte", 
+        description: "Basic information about Le Relais De L’Entrecôte" 
+    }, 
+    { 
+        title: "Cedric Grolet",  
+        image: "images/croissant.jpeg", 
+        link: "About Cedric Grolet", 
+        description: "Basic information about Cedric Grolet"
+    }
+]; 
 
 
-app.get('/api/paris', (req, res) => {
-    res.json({ statusCode:200, data: cardList, message: "Success" });
+let collection;
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.json()); app.use(express.urlencoded({extended: false}));
+
+
+// MongoClient with MongoClient object for stable API
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        useNewUrlParser: true,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
+async function runDBConnection() {
+    try {
+        await client.connect();
+        collection = client.db('Restaurants').collection('Paris');
+        console.log("MongoDB connected successfully!");
+    } catch (ex) {
+        console.error("Failed to connect to MongoDB", ex);
+    }
+}
 
+runDBConnection();
+
+app.get('/', function (req,res) {
+    res.render('index.html');
+});
+
+app.get('/api/paris', (req, res) => {
+    res.json({ statusCode: 200, data: cardList, message: "Success" })
+});
+
+app.get('/', async (req, res) => {
+    getAllParis((err, result) => {
+        if (!err) {
+            res.json({ statusCode: 200, data: result, message: "Retrieved all Paris data successfully" });
+        } else {
+            console.error("Error fetching Paris data:", err);
+            res.status(500).json({ statusCode: 500, message: "Error" });
+        }
+    });
+});
+
+app.post('/api/paris', (req, res) => {
+    let Paris = req.body;
+     collection.insertOne(Paris, (err, result) => {
+        if (!err) {
+            res.status(201).json({ statusCode: 201, data: result.ops, message: "Paris data added successfully" });
+        } else {
+            console.error("There was an error in posting Paris data:", err);
+            res.status(500).json({ statusCode: 500, message: "Error" });
+        }
+    });
+});
+
+function postParis(parisData, callback) {
+    collection.insertOne(parisData, (err, result) => {
+        if (err) {
+            console.error("Error inserting Paris data:", err);
+            callback(err);
+        } else {
+            callback(null, result.ops); 
+        }
+    });
+}
 
 
 app.listen(port, ()=>{
